c49374ad6ce046937de105bf9680459cb0402fd8
MAHBUBUL ALAM <33549330+promahbubul@users.noreply.github.com>
2024-02-17 23:43:16 +0600

    CV1 created

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,22 @@
-import { 
-  HashRouter, 
-  Routes, 
-  Route
-} from "react-router-dom";
-import Verify from "./pages/verify"
-import Editor from "./pages/editor"
-import NotFound from "./pages/notFound"
-import Dev from "./pages/dev"
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Verify from "./pages/verify";
+import Editor from "./pages/editor";
+import NotFound from "./pages/notFound";
+import Dev from "./pages/dev";
+import CV1 from "./pages/cv1";
 
 function App() {
   return (
     <HashRouter>
       <Routes>
-        <Route path="/download/:_id" element={<Verify/>} /> 
-        <Route path="/editor" element={<Editor/>} /> 
-        <Route path="/dev" element={<Dev/>} /> 
+        <Route path="/download/:_id" element={<Verify />} />
+        <Route path="/editor" element={<Editor />} />
+        <Route path="/dev" element={<Dev />} />
+        <Route path="/cv1" element={<CV1 />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </HashRouter>
-  )
+  );
 }
 
-export default App
-
-
-
+export default App;
--- src/assets/Images/Images.jsx
+++ src/assets/Images/Images.jsx
@@ -1,3 +1,10 @@
 import avatar from "./avatar.jpg";
+import telephone from "./telephone.png";
+import email from "./email.png";
+import screen from "./screen.png";
+import home from "./home.png";
+import facebook from "./facebook-app-symbol.png";
+import linkedIn from "./linkedin.png";
+import twitter from "./twitter.png";
 
-export { avatar };
+export { avatar, telephone, email, screen, home, facebook, linkedIn, twitter };

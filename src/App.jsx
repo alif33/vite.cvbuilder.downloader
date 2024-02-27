cf9169c2b52a739ae78e468498304eb2fcdea741
MAHBUBUL ALAM <33549330+promahbubul@users.noreply.github.com>
2024-02-27 23:20:34 +0600

    cv1

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Editor from "./pages/editor";
 import NotFound from "./pages/notFound";
 import Dev from "./pages/dev";
 import CV1 from "./pages/cv1";
+import Cv2 from "./pages/CV2";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/editor" element={<Editor />} />
         <Route path="/dev" element={<Dev />} />
         <Route path="/cv1" element={<CV1 />} />
+        <Route path="/cv2" element={<Cv2 />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </HashRouter>
--- src/assets/Images/Images.jsx
+++ src/assets/Images/Images.jsx
@@ -6,5 +6,24 @@ import home from "./home.png";
 import facebook from "./facebook-app-symbol.png";
 import linkedIn from "./linkedin.png";
 import twitter from "./twitter.png";
+import facebook2 from "./cv2/facebook.png";
+import email2 from "./cv2/email.png";
+import instagram2 from "./cv2/instagram.png";
+import phone2 from "./cv2/phone.png";
+import twitter2 from "./cv2/twitter.png";
 
-export { avatar, telephone, email, screen, home, facebook, linkedIn, twitter };
+export {
+  avatar,
+  telephone,
+  email,
+  screen,
+  home,
+  facebook,
+  linkedIn,
+  twitter,
+  facebook2,
+  email2,
+  instagram2,
+  phone2,
+  twitter2,
+};

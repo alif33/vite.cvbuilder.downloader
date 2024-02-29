14ea6edfeab892fd2c293a3aed931e81aea7b3a6
ismail alif <43023388+alif33@users.noreply.github.com>
2024-02-29 11:27:32 +0600

    download page fixed

--- src/pages/download/index.jsx
+++ src/pages/download/index.jsx
@@ -1,19 +1,17 @@
-import { PDFDownloadLink, Document, Page, Text } from '@react-pdf/renderer'
+import { PDFDownloadLink } from '@react-pdf/renderer'
 import CV from "../../pages/cv1"
-
-// const MyDoc = () => (
-//   <Document>
-//     <Page>
-//       <Text>Ismail h alif</Text>
-//     </Page>
-//   </Document>
-// )
+import Button from '../../ui/button/download'
+import PulseLoader from "react-spinners/PulseLoader"
+import Header from '../../components/header'
 
 const App = () => (
-  <div>
-    <PDFDownloadLink document={<CV />} fileName="somename.pdf">
-      {({ blob, url, loading, error }) => (loading ? 'Loading document...' : 'Download now!')}
-    </PDFDownloadLink>
+  <div className="min-h-screen">
+      <Header/>
+      <div className="flex justify-center mt-40">
+        <PDFDownloadLink document={<CV />} fileName="somename.pdf">
+          {({ loading }) => (loading ? <PulseLoader color="#36d7b7" /> : <Button>Download Pdf</Button>)}
+        </PDFDownloadLink>
+      </div>
   </div>
 )
 
